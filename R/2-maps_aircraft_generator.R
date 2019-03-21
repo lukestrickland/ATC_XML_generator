f6d8541743f098685d6e4ae95743db4d6e9a2dcf
Luke Strickland <00079987@uwa.edu.au>
2019-03-21 09:42:35 +0800

    Restructure

--- R/2-maps_aircraft_generator.R
+++ R/2-maps_aircraft_generator.R
@@ -21,19 +21,16 @@ session=1
 for (p in participant_number){
   for (c in 1:n_conds){
 
-  exp_var_df <- create_exp_var_df(nPairs=240, callsigns=callsigns)
-  
-  
-  sim_input_df <- create_sim_input_df(exp_var_df = exp_var_df, aspectRatio = 0.625, x_dim=180)
-  
-  maps_and_ac <- create_xml_ac_and_maps("auto", exp_var_df, sim_input_df)
-  
-  writeLines(maps_and_ac, paste('atc_09_maps_and_ac_p', 
-                                participant_number[p], '_', 's', 
-                                session, '_', 'auto', '_', 
-                                '1', '.xml', sep = ''), 
-                                                    sep = '\n\n')
-
+    exp_var_df <- create_exp_var_df(nPairs=240, callsigns=callsigns)
+    sim_input_df <- create_sim_input_df(exp_var_df = exp_var_df,
+                                        aspectRatio = 0.625, x_dim=180)
+    maps_and_ac <- create_xml_ac_and_maps("auto", exp_var_df, sim_input_df)
+    
+    writeLines(maps_and_ac, paste('atc_09_maps_and_ac_p', 
+                                  participant_number[p], '_', 's', 
+                                  session, '_', 'auto', '_', 
+                                  '1', '.xml', sep = ''), 
+                                                      sep = '\n\n')
   }
 }
 
